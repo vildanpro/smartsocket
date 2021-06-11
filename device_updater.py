76e34c316c7434b1814032338b13ed17627eb09b
Victor <vildanpro@gmail.com>
2021-06-11 15:16:21 +0500

    qqq

--- device_updater.py
+++ device_updater.py
@@ -7,7 +7,7 @@ def clean_leases(leases_list):
     leases_cleaned = list()
     for lease_raw in leases_list:
         if int(lease_raw['address'].split('.')[3]) >= 100:
-            leases_cleaned.append({'mac': lease_raw['mac-address'].upper(), 'ip': lease_raw['address']})
+            leases_cleaned.append({'MAC': lease_raw['mac-address'].upper(), 'IP': lease_raw['address']})
     return leases_cleaned
 
 
@@ -20,11 +20,11 @@ if __name__ == '__main__':
         leases = clean_leases(leases_raw)
         while leases:
             lease = leases.pop()
-            device_db = select_device(lease['mac'])
+            device_db = select_device(lease['MAC'])
             if device_db:
-                if device_db[0]['ip'] != lease['ip']:
-                    update_device_ip(device_db[0]['mac'], lease['ip'])
-                    print(f'Updated device {device_db[0]["device_name"]}')
+                if device_db[0]['IP'] != lease['IP']:
+                    update_device_ip(device_db[0]['MAC'], lease['IP'])
+                    print(f'Updated device {device_db[0]["DEVICE_NAME"]}')
             else:
                 insert_device(mac=lease['mac'], ip=lease['ip'])
         sleep(5)
