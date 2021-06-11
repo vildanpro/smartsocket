322c2b0251da38a065284d2141f41f0a696b316b
Victor <vildanpro@gmail.com>
2021-06-11 15:13:26 +0500

    q

--- device_updater.py
+++ device_updater.py
@@ -1,6 +1,4 @@
-import subprocess
 from time import sleep
-
 from queries import update_device_ip, select_device, insert_device
 from mikrotik_api import get_dhcp_leases
 
@@ -25,8 +23,8 @@ if __name__ == '__main__':
             device_db = select_device(lease['mac'])
             if device_db:
                 if device_db[0].ip != lease['ip']:
-                    update_device_ip(device_db[0].mac, lease['ip'])
-                    print(f'Updated device {device_db[0].device_name}')
+                    update_device_ip(device_db[0]['mac'], lease['ip'])
+                    print(f'Updated device {device_db[0]["device_name"]}')
             else:
                 insert_device(mac=lease['mac'], ip=lease['ip'])
         sleep(5)
