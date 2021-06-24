14760dcb9ac41c870ec2ad7bc8ef451b66603fc0
Victor <vildanpro@gmail.com>
2021-06-24 12:11:57 +0500

    refactor off_all_devices.py

--- off_all_devices.py
+++ off_all_devices.py
@@ -1,9 +1,15 @@
-from queries import insert_new_message, select_devices_ids
+from queries import insert_new_message, select_device_by_mac
+from mikrotik_api import get_dhcp_leases
+from pprint import pprint
 
 
-devices = select_devices_ids()
+leases = get_dhcp_leases()
+pprint(leases)
+devices = list()
+for lease in leases:
+    devices.append(select_device_by_mac(lease['mac-address']))
+pprint(devices)
 for device in devices:
     insert_new_message(device_id=device['DEVICE_ID'],
                        message_type_id=22,
                        message_state_id=1)
-
