4bb0d0565fe7118a36949304cefd90f12b886f85
Victor <vildanpro@gmail.com>
2021-07-01 15:50:34 +0500

    add flask app

--- flask_app.py
+++ flask_app.py
@@ -1,42 +1,46 @@
-from flask import Flask, render_template_string, request
-from db import get_messages_types
+from flask import Flask, render_template_string, request, redirect, url_for
+from db import DB
+from send_messages_to_devices import add_messages
+from mikrotik_api import get_dhcp_leases
 
 
 app = Flask(__name__)
-
+db = DB()
 
 template = """
     <!DOCTYPE html>
     <html lang="en">
     <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <meta charset="UTF-8">
-        <title>Title</title>
+        <title>SmartSocket</title>
     </head>
     <body>
-        {% for message_type_id in message_types_id %}
-        
-            <input type="submit" name="{{message_type_id.message_type_id}}" 
-            value="{{message_type_id.message_type_name}}">
-        {% endfor %}
+        <form action='/'>
+            {% for message_type_id in message_types %}
+                <input type="submit" name='{{message_type_id['MESSAGE_TYPE_ID']}}' 
+                value='{{message_type_id['MESSAGE_TYPE_NAME']}}' placeholder='{{message_type_id['DESCRIPTION']}}'>
+                <br>
+            {% endfor %}
+        </form>
     </body>
     </html>
 """
 
 
-@app.route("/", methods=['GET', 'POST'])
-def control():
-    if request.method == 'POST':
-        if request.form['submit_button'] == 'Do Something':
-            pass  # do something
-        elif request.form['submit_button'] == 'Do Something Else':
-            pass  # do something else
-        else:
-            pass  # unknown
-    elif request.method == 'GET':
-        message_types_id = get_messages_types()
-        return render_template_string(template, context=message_types_id)
+@app.route("/")
+def index():
+    message_types = db.get_messages_types()
+    for message_type in message_types:
+        message_type_id = message_type['MESSAGE_TYPE_ID']
+        if request.args.get(str(message_type_id)):
+            add_messages(get_dhcp_leases(), message_type_id)
+            return redirect(url_for('index'))
+
+    return render_template_string(template, message_types=message_types)
 
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='192.168.93.35')
+    app.run(host='192.168.93.90')
