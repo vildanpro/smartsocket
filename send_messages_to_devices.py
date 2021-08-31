76673f4c5d380dd3483f1e293454826c1d62d874
Victor <vildanpro@gmail.com>
2021-08-31 16:17:26 +0500

    rename main

--- send_messages_to_devices.py
+++ send_messages_to_devices.py
@@ -3,16 +3,6 @@ from db import DB
 db = DB()
 
 
-def add_messages(leases, message_type_id):
-    devices = list()
-    for lease in leases:
-        devices.append(db.select_device_by_mac(lease['mac-address']))
-    for i, device in enumerate(devices, start=1):
-        db.insert_new_message(device_id=device['DEVICE_ID'],
-                              message_type_id=message_type_id)
-        print(f'{i}. Insert message: device_id {device["DEVICE_ID"]}, message_type_id {message_type_id}')
-
-
 def add_message(device, message_type_id):
     db.insert_new_message(device_id=device['DEVICE_ID'],
                           message_type_id=message_type_id)
@@ -20,10 +10,7 @@ def add_message(device, message_type_id):
 
 
 if __name__ == '__main__':
-    message_types_id = db.get_messages_types()
-    if not message_types_id:
-        print('No states, exit.')
-    for message_type in message_types_id:
-        print(message_type['MESSAGE_TYPE_ID'], message_type['DESCRIPTION'])
-    message_type_id = input('\nВыберите тип соообщения для устройств: ')
-    add_messages(get_dhcp_leases(), message_type_id)
+    devices = db.get_devices()
+    while True:
+        for device in devices:
+            add_message(device, '41')
