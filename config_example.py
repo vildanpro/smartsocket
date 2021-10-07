ed8e5db98c93f3ad56e25e0e096dd9e64faa1872
Victor <vildanpro@gmail.com>
2021-10-07 11:37:20 +0500

    add config_example.py

---
+++ config_example.py
@@ -0,0 +1,10 @@
+# ORACLE DB
+oracle_credentials = {'user': 'username', 'password': 'password', 'dsn': 'ip/database', 'encoding': 'UTF-8'}
+# MONGO DB
+mongo_credentials = {'db': 'db', 'host': 'host', 'port': 27017}
+
+device_request_timeout = 60
+process_timeout = 3
+
+
+
